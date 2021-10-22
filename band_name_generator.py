04c1c4a4932d13e34b145db35bc0cf08d24ddb2b
livingonGH325 <ysrinivasa1998@gmail.com>
2021-10-22 22:16:16 +0530

    Day_1

---
+++ band_name_generator.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+
+def main():
+    
+    print("This program will help you make up a potential band name" + "\n")
+
+    place_name = input("Where do you live?" + "\n")
+    pet_name = input("What was your pet's name?" + "\n")
+
+    print("Your potential band name could be " + str(place_name) + ' ' + str(pet_name) + '\n')
+
+    print("Have fun!")
+
+if __name__ == "__main__":
+    main()
+
+    
